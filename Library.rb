51eafc7c1d67e827d8dd417bccf71bfcd7f926a0
prudnikovv <prudnikovv@ukr.net>
2022-07-21 06:33:38 +0000

    Add methods add_ to class Library

--- Library.rb
+++ Library.rb
@@ -2,14 +2,34 @@ require '/Classes.Author'
 require '/Classes.Book'
 require '/Classes.Reader'
 require '/Classes.Order'
+require "./Module/Validation"
 
 require 'yaml'
 
 class Library
-  @@books = []
-  @@readers = []
-  @@orders = []
+  include Validation
   def initialize
+    load_data
+    @books = []
+    @readers = []
+    @orders = []
+   end
+
+  privat
+
+  def add_book(book)
+    is_a_book(book)
+    @books << book
+  end
+
+  def add_reader(reader)
+    is_a_reader(reader)
+    @readers << reader
+  end
+
+  def add_order(order)
+    is_a_order(order)
+    @orders << order
   end
 
 end
